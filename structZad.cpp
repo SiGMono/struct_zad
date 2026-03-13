747130fa34f68c955fc3c5fe979ed48a94339dbd
SiGMono <91075739+SiGMono@users.noreply.github.com>
2026-03-13 12:53:10 +0100

    Add file

---
+++ structZad.cpp
@@ -0,0 +1,16 @@
+/*
+    Szymon Zeller Informatyka1
+    PRogram do czytania i wpisywania structów do pliku .txt
+*/
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
